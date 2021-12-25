714f9ab2a84250d87a8b97c098f393e880c22d49
Junho Park <junhopark@Junhos-MacBook-Pro.local>
2021-12-25 18:01:25 +0900

    setting_reset,router

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
-import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/:id" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
